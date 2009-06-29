567574197463bf3d1ea072b4024af4bfc1df4a47
bnulwh <bnulwh@eba99256-646a-11de-8ac7-2bb2ed267d8e>
2009-06-29 08:52:42 +0000

    The first release of my own ChineseSegment

---
+++ trunk/Lwh.ChineseSegment/DictionaryLoader/BinDictionaryLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lwh.ChineseSegment.Utility;
+
+namespace Lwh.ChineseSegment.DictionaryLoader
+{
+    /// <summary>
+    /// bin词库加载器,存于bin文件中。
+    /// </summary>
+    public class BinDictionaryLoader : IDictionaryLoader
+    {
+        public BinDictionaryLoader()
+        {
+        }
+
+        #region IDictionaryLoader 成员
+
+
+        public List<string> Load(string dictionaryPath)
+        {
+            if (!Validator.IsValidFile(dictionaryPath))
+            {
+                return new List<string>();
+            }
+
+            return Serialization.DeserializeBin(dictionaryPath, typeof(List<string>)) as List<string>;
+        }
+
+        #endregion
+    }
+}
